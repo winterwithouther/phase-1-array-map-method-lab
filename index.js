9425146f69d98a9aabf2725a83be1058d1c3084b
Eman Gurung <egurung1117@gmail.com>
2023-08-05 03:30:59 -0400

    complete

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  const newTutorials = tutorials.map((phrase) => {
+    let newPhrase = "";
+  
+    newPhrase += phrase[0].toUpperCase();
+  
+    for (let i = 1; i < phrase.length; i++) {
+      if (phrase[i] === " ") {
+        newPhrase += phrase[i];
+        newPhrase += phrase[i + 1].toUpperCase();
+        i++;
+      } else {
+        newPhrase += phrase[i];
+      }
+    }
+  
+    return newPhrase;
+  })
+
+  return newTutorials
 }
+
+
